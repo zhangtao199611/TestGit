3904c12f8b20a12428390be8fc6c7638835533a6
zt <1575400367@qq.com>
2022-02-20 01:56:38 +0800

    test2022-02-20-01-53

--- src/HiWoorId.java
+++ src/HiWoorId.java
@@ -2,5 +2,6 @@ public class HiWoorId {
     public static  void main(String args[]){
         System.out.printf("hi worId");
         System.out.printf("sss");
+        System.out.printf("sss");
     }
 }
